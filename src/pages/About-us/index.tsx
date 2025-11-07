a107359f1e34b3b9a5a20051217c12eb9ff2532f
Ravi <spinachstudiosm27@Spinachs-MacBook-Pro.local>
2025-11-07 12:24:42 +0530

    add folders

---
+++ src/pages/About-us/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="text-desktop-heading-h1 font-playfair-display italic text-center">
+      AboutUs
+    </div>
+  );
+};
+
+export default AboutUs;
