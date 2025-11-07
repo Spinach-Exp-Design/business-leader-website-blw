cfd86416c3d0db2bb9211f938c4a323fa64b3363
HarshSpinach <harsh@spinachexperience.design>
2025-11-07 12:35:04 +0530

    Add clsx dependency, implement useDeviceType hook, and update styles
    - Added clsx as a dependency in package.json and package-lock.json.
    - Created a new custom hook, useDeviceType, to determine the device type based on window width.
    - Updated globals.css with comments for breakpoints.
    - Modified layout.tsx to suppress hydration warnings.

---
+++ src/hooks/useDeviceType.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { useState, useLayoutEffect } from "react";
+
+const useDeviceType = () => {
+  const [deviceType, setDeviceType] = useState({
+    isMobile: false,
+    isTablet: false,
+    isLaptop: false,
+    isDesktop: false,
+  });
+
+  useLayoutEffect(() => {
+    const getDeviceType = () => {
+      const width = window?.innerWidth;
+      return {
+        isMobile: width <= 767,
+        isTablet: width >= 768 && width <= 1023,
+        isLaptop: width >= 1024 && width <= 1279,
+        isDesktop: width >= 1280,
+      };
+    };
+
+    let throttleTimeout: number | null = null;
+
+    const handleResize = () => {
+      if (!throttleTimeout) {
+        throttleTimeout = requestAnimationFrame(() => {
+          const newDeviceType = getDeviceType();
+          setDeviceType((prev) => {
+            // Update only if device type changes
+            if (
+              prev?.isMobile === newDeviceType?.isMobile &&
+              prev?.isTablet === newDeviceType?.isTablet &&
+              prev?.isLaptop === newDeviceType?.isLaptop &&
+              prev?.isDesktop === newDeviceType?.isDesktop
+            ) {
+              return prev;
+            }
+            return newDeviceType;
+          });
+          throttleTimeout = null;
+        });
+      }
+    };
+
+    // Set initial device type on mount
+    handleResize();
+
+    window?.addEventListener("resize", handleResize);
+    return () => {
+      window?.removeEventListener("resize", handleResize);
+      if (throttleTimeout) {
+        cancelAnimationFrame(throttleTimeout);
+      }
+    };
+  }, []);
+
+  return deviceType;
+};
+
+export default useDeviceType;
