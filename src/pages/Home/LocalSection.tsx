252cc4a54489e619c8ecd15efa31666f3ba884d4
Ravi <spinachstudiosm27@Spinachs-MacBook-Pro.local>
2025-11-10 18:59:41 +0530

    added local desktop design

--- src/pages/Home/LocalSection.tsx
+++ src/pages/Home/LocalSection.tsx
@@ -31,13 +31,30 @@ const LocalSection = () => {
             Know more about Rajesh
           </PrimaryButton>
         </div>
-        <div className="h-149 w-119">
+        <div className="h-149 w-119 z-1">
           <img
             src={localSectionData.image}
             alt="Rajesh Krishnamoorthy"
             className="w-full h-full object-cover"
           />
         </div>
+      </div>
+      <div className="bg-primary-yellow py-14 pl-39 -mt-44 mr-7 relative z-0">
+        <div className="flex gap-2 absolute -top-8 left-6">
+          <img
+            src="/assets/icons/white-quote.svg"
+            alt="White quote"
+            className="w-14 h-22"
+          />
+          <img
+            src="/assets/icons/white-quote.svg"
+            alt="White quote"
+            className="w-14 h-22"
+          />
+        </div>
+        <h2 className="text-desktop-quote-2 font-playfair-display italic text-primary-dark w-115.5">
+          {localSectionData.quote}
+        </h2>
       </div>
     </div>
   );
